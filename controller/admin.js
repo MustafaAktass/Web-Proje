72c8a2056cfbecc531334d9dd2716a0198fa65e5
MustafaAktass <gokberkmustafa@outlook.com>
2024-05-14 01:05:33 +0300

    Delete ve Update işlemi tamamlandı

--- controller/admin.js
+++ controller/admin.js
@@ -1,6 +1,5 @@
 const upload = require("../middleware/fileUpload")
 const ShopData = require("../model/shopdata")
-const { ObjectId } = require('mongodb');
 exports.processIndustrialShop = async(req,res)=>{
     try{
     upload(req,res,async(err)=>{
@@ -26,7 +25,8 @@ exports.processIndustrialShop = async(req,res)=>{
                 });
 
                 const savedShop = await data.save();
-                res.json(savedShop);
+                // res.json(savedShop);
+                res.redirect('/admin/add-industrial-shop');
         }
     })}
     catch(err){
@@ -54,9 +54,53 @@ exports.deleteIndustrialShop = async (req, res, next) => {
         if (!data) {
             return res.status(404).send('İşletme bulunamadı veya zaten silinmiş');
         }
-        req.method = 'GET';
-        res.redirect(303,'admin/list-industrial-shop');
+        res.redirect('/admin/list-industrial-shop');
     } catch (err) {
         return res.status(500).send(`İşletme silinirken bir hata oluştu: ${err.message}`);
+    }
+}
+exports.editIndustrialShop = async (req, res, next) => {
+    try {
+        const data = await ShopData.findById(req.params.id);
+        if (!data) {
+            return res.status(404).send('İşletme bulunamadı');
+        }
+        res.render('admin/edit-industrial-shop', { shopdata: data });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+exports.updateIndustrialShop = async (req, res, next) => {
+    try {
+        upload(req,res,async(err)=>{
+            if(err){
+                return res.status(400).json({ message: 'Dosya yükleme hatası: ' + err.message });
+            }
+            else{
+                const { IsletmeAdi, Kategori, Adres, IletisimBilgileri, Sehir, Aciklama } = req.body;
+                    
+                    // Yüklenen her dosya için dosya yollarını alıp resimler dizisine ekleyelim
+                    const resimler = req.files.map(file => {
+                        return { dosyaYolu: file.path };
+                    });
+        
+                    const updatedShop = await ShopData.findByIdAndUpdate(req.params.id, {
+                        IsletmeAdi,
+                        Kategori,
+                        Adres,
+                        IletisimBilgileri,
+                        Sehir,
+                        Aciklama,
+                        resimler: resimler
+                    }, { new: true });
+                    if (!updatedShop) {
+                        return res.status(404).send('İşletme bulunamadı');
+                    }
+            
+                    res.redirect('/admin/list-industrial-shop');
+            }
+        })
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,10 @@ router.get('/',adminController.homePage);
 router.get('/add-industrial-shop',adminController.addIndustrialShop);
 router.post('/add-industrial-shop',adminController.processIndustrialShop);
 router.get('/list-industrial-shop',adminController.listIndustrialShop);
-router.delete('/list-industrial-shop/:id',adminController.deleteIndustrialShop);
+router.post('/list-industrial-shop/:id',adminController.deleteIndustrialShop);
+router.get('/edit-industrial-shop/:id', adminController.editIndustrialShop);
+router.post('/edit-industrial-shop/:id', adminController.updateIndustrialShop);
+
+
 
 module.exports = router;
